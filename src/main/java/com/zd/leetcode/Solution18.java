87c26f304b33558aee15062175c69002edd9e3ac
zhengdai <798484315@qq.com>
2020-03-26 11:51:10 +0800

    feat: 完成solution18

--- src/main/java/com/zd/leetcode/Solution18.java
+++ src/main/java/com/zd/leetcode/Solution18.java
@@ -16,8 +16,14 @@ public class Solution18 {
             return result;
         }
         Arrays.sort(nums);
-        for (int i = 0; i < n - 4; ++i) {
-            for (int j = i + 1; j < n - 3; ++j) {
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
                 int l = j + 1;
                 int r = n - 1;
                 while (l < r) {
--- src/test/java/com/zd/leetcode/Solution18Test.java
+++ src/test/java/com/zd/leetcode/Solution18Test.java
@@ -28,5 +28,9 @@ public class Solution18Test {
         assertThat(res.get(0), is(Arrays.asList(-2, -1, 1, 2)));
         assertThat(res.get(1), is(Arrays.asList(-2,  0, 0, 2)));
         assertThat(res.get(2), is(Arrays.asList(-1,  0, 0, 1)));
+        List<List<Integer>> res2 = solution18.fourSum(new int[]{0, 0, 0, 0}, 0);
+        assertThat(res2.get(0), is(Arrays.asList(0, 0, 0, 0)));
+        List<List<Integer>> res3 = solution18.fourSum(new int[]{-3, -2, -1, 0, 0, 1, 2, 3}, 0);
+        System.out.println(res3);
     }
 }
