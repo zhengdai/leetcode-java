49030245e02ea8cb6a9a0f3eddc3e6a9b14f8edb
zhengdai <798484315@qq.com>
2020-11-24 20:07:39 +0800

    fix: 完成solution42

--- src/main/java/com/zd/leetcode/Solution42.java
+++ src/main/java/com/zd/leetcode/Solution42.java
@@ -1,10 +1,45 @@
 package com.zd.leetcode;
 
+import java.util.Stack;
+
 /**
  * @author zhengdai
  * @date 2020年04月29日
  */
 public class Solution42 {
+
+    //按行求
+    public int trapWithRow(int[] height) {
+        int sum = 0;
+        int max = getMax(height);
+        for (int i = 1; i <= max; ++i) {
+            boolean isStart = false;
+            int temp_sum = 0;
+            for (int j = 0; j <height.length; ++j) {
+                if (isStart && height[j] < i) {
+                    temp_sum++;
+                }
+                if (height[j] >= i) {
+                    sum += temp_sum;
+                    temp_sum = 0;
+                    isStart = true;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private int getMax(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; ++i) {
+            if (height[i] > max) {
+                max = height[i];
+            }
+        }
+        return max;
+    }
+
+    // 动态规划
     public int trap(int[] height) {
         if (height == null) {
             return 0;
@@ -24,9 +59,87 @@ public class Solution42 {
         for (int i = len - 2; i >= 0; --i) {
             rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
+        // len - 2次循环
         for (int i = 1; i < len - 1; i++) {
             ans += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
         return ans;
     }
+
+    // 动态规划优化
+    public int trap1(int[] height) {
+        if (height == null) {
+            return 0;
+        }
+        int sum = 0;
+        int len = height.length;
+        if (len == 0) {
+            return sum;
+        }
+        int max_left = 0;
+        int[] max_right = new int[len];
+        for (int i = len - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < len - 1; ++i) {
+            max_left = Math.max(max_left, height[i - 1]);
+            int min = Math.min(max_left, max_right[i]);
+            if (min > height[i]) {
+                sum += min - height[i];
+            }
+        }
+        return sum;
+    }
+
+    // 动态规划优化
+    public int trap2(int[] height) {
+        int sum = 0;
+        int max_left = 0;
+        int max_right = 0;
+        int left = 1;
+        // 加右指针进去
+        int right = height.length - 2;
+        for (int i = 1; i < height.length - 1; i++) {
+            //从左到右更
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                int min = max_left;
+                if (min > height[left]) {
+                    sum = sum + (min - height[left]);
+                }
+                left++;
+                //从右到左更
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                int min = max_right;
+                if (min > height[right]) {
+                    sum = sum + (min - height[right]);
+                }
+                right--;
+            }
+        }
+        return sum;
+    }
+
+    public int trap3(int[] height) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        int current = 0;
+        while (current < height.length) {
+            while (!stack.empty() && height[current] > height[stack.peek()]) {
+                int h = height[stack.peek()];
+                stack.pop();
+                if (stack.empty()) {
+                    break;
+                }
+                int distance = current - stack.peek() - 1;
+                int min = Math.min(height[stack.peek()], height[current]);
+                sum = sum + distance * (min - h);
+            }
+            stack.push(current);
+            current++;
+        }
+        return sum;
+    }
+
 }
--- src/test/java/com/zd/leetcode/Solution42Test.java
+++ src/test/java/com/zd/leetcode/Solution42Test.java
@@ -23,4 +23,29 @@ public class Solution42Test {
     public void testTrap() {
         assertEquals(6, solution42.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
+
+    @Test
+    @DisplayName("test trap1")
+    public void testTrap1() {
+        assertEquals(6, solution42.trap1(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    @Test
+    @DisplayName("test trap2")
+    public void testTrap2() {
+        assertEquals(6, solution42.trap2(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    @Test
+    @DisplayName("test trap3")
+    public void testTrap3() {
+        assertEquals(6, solution42.trap3(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    @Test
+    @DisplayName("test trapWithRow")
+    public void testTrapWithRow() {
+        assertEquals(6, solution42.trapWithRow(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
 }
